0bbd0b57080a7e6379b068ee6aaf37e30b5f9b6d
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-04-29 06:33:14 +0800

    ChallengeExpandText

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from "./StarRating";
 import StarRatingVersionOne from './StarRatingVersionOne';
+import ChallengeTextExpander from './ChallengeTextExpander';
 // import './index.css';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={6} />
-    {/* <StarRatingVersionOne /> */}
-  </React.StrictMode>
+  <div>
+      {/* <App /> */}
+      {/* <StarRating maxRating={6} /> */}
+      {/* <StarRatingVersionOne /> */}
+      <ChallengeTextExpander />
+    </div>
+
 );
