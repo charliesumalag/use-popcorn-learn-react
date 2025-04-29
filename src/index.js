c32a9b370ecdb75b0a2b85209c70c9c49ead579c
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-04-30 05:54:14 +0800

    loading sate, aync, useeffect, fetch

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import StarRating from "./StarRating";
 import StarRatingVersionOne from './StarRatingVersionOne';
 import ChallengeTextExpander from './ChallengeTextExpander';
 // import './index.css';
-// import App from './App';
+import App from './App';
+import AppVersion from './AppVersion';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
       {/* <App /> */}
+      <AppVersion />
       {/* <StarRating maxRating={6} /> */}
       {/* <StarRatingVersionOne /> */}
-      <ChallengeTextExpander />
+      {/* <ChallengeTextExpander /> */}
     </div>
 
 );
