6085c370877b364490446d0b2ce068279bec4f93
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-05-01 06:08:28 +0800

    Loading Movie Details

--- src/AppVersion.js
+++ src/AppVersion.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useState } from "react";
 import { tempMovieData, tempWatchedData } from "./movieData";
+import StarRating from "./StarRating";
 
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -96,10 +97,45 @@ function Movie({movie, onSelectMovie}) {
 }
 
 function MovieDetails({selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const {Title: title, Year: year, Poster: poster, Runtime: runtime, imdbRating, Plot: plot, Released: released, Actors: actors, Director: director, Genre: genre} = movie;
+  useEffect(() => {
+    async function getMovieDetails(params) {
+      setIsLoading(true);
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+      const data = await res.json();
+      setMovie(data);
+      setIsLoading(false);
+    }
+    getMovieDetails();
+  },[selectedId]);
+
   return (
     <div className='details'>
-      <button className='btn-back' onClick={onCloseMovie}>&larr;</button>
-      {selectedId}
+      {isLoading ? <Loader /> :
+      <>
+      <header>
+        <button className='btn-back' onClick={onCloseMovie}>&larr;</button>
+        <img src={poster} alt={`Poster of ${movie}`} />
+        <div className="details-overview">
+          <h2> {title} </h2>
+          <p>{released} &bull; {runtime}</p>
+          <p>{genre}</p>
+          <p><span>⭐</span>{imdbRating} IMDb rating</p>
+        </div>
+        </header>
+
+        <section>
+          <div className='rating'>
+            <StarRating maxRating={10} size={24} />
+          </div>
+          <p><em>{plot}</em></p>
+          <p>Starring {actors}</p>
+          <p>Directed by {director}</p>
+        </section>
+        </>
+        }
     </div>
   )
 
