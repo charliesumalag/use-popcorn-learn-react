73deee5a7e87f74c22a5e7544df122086a16f122
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-05-03 08:06:21 +0800

    ref to select dom elements

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 
 
 const Search = ({query, setQuery}) => {
+
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === 'Enter') {
+        if (document.activeElement === inputEl.current) return;
+        inputEl.current.focus();
+        setQuery('');
+      }
+    }
+    document.addEventListener('keydown', callback)
+    return () => document.removeEventListener('keydown', callback);
+  },[setQuery])
     return (
-        <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)}/>
+        <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} ref={inputEl} />
       )
 }
 
